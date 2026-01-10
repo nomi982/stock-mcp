8de390ccf7a390542f0fa1dd380c5fa75b9e4d65
nomi982 <ruci01203@gmail.com>
2026-01-10 13:53:05 +0800

    更新requirements.txt

--- 提交Git.bat
+++ 提交Git.bat
@@ -1,5 +1,8 @@
+@echo off
+chcp 65001 >nul
+
 git config --global core.autocrlf true
 git add .
-git commit -m "添加requirements.txt"
+git commit -m "更新requirements.txt"
 git branch -M main
 git push -u origin main
